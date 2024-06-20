cb3133e9c94e4f9b8e82ee9e0ff45c7355507a69
jagriti <im.jagriti.jha@gmail.com>
2024-06-20 23:24:18 +0530

    added a screen to preview the marksheet pdf

---
+++ lib/views/pdf_preview_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+
+class PdfPreviewScreen extends StatelessWidget {
+  const PdfPreviewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String path = ModalRoute.of(context)!.settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PDF Preview'),
+      ),
+      body: path.isNotEmpty
+          ? Padding(
+              padding: const EdgeInsets.all(24),
+              child: PDFView(
+                filePath: path!,
+              ),
+            )
+          : const Center(child: Text("PDF file not found")),
+    );
+  }
+}
